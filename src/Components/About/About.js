bdac7f67d615f8d6038ba4ae4d02ef0a738e4a62
Lili Fuentes <68561576+LFuen@users.noreply.github.com>
2023-06-02 19:49:51 -0400

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -12,12 +12,12 @@ class About extends Component{
             <h1 className='flexRow' id='About'>About</h1>
             
             <div className='flexColumn'>
-            <p className='item'>I've been a massage therapist for a decade. No, that has nothing to do with programming, HOWEVER, the opportunity to dive 
-                into coding and make it a career almost literally fell into my lap. Thanks to COVID (remember that?), I had a mind-shift and decided I would  
-                do something that would challenge and intellectually stimulate me. I've had various career lines, from teaching biology, coaching high school softball, 
-                real estate underwriting, and even a professional wolverine wrangler. Ok, that last one was just a test to see if you were still with me! 
-                But it would <i>totally</i> be exciting, not to mention absolutely terrifying! I love collaborating in every job that I've ever had, 
-                and I would love the chance to pick the brains of great developers and coders out there!</p>
+            <p className='item'>I have been a software engineer for nearly 3 years and a massage therapist for more than a decade now. The opportunity 
+                to dive into coding and make it a career almost literally fell into my lap. Thanks to COVID (remember that?), I had a mind-shift 
+                and decided I would do something that would challenge and intellectually stimulate me. I've had various career lines, from 
+                teaching biology, coaching high school softball, real estate underwriting, and even a professional wolverine wrangler. Ok, 
+                that last one was just a test to see if you were still with me! But it would <i>totally</i> be exciting, not to mention 
+                absolutely terrifying! I love collaborating in every job that I've ever had, and that truth continues for me as an engineer.</p>
                 <p><b>Services I offer include, but are not limited to:</b></p>
             </div>
             
@@ -28,10 +28,9 @@ class About extends Component{
                 <p><b>SEO</b></p>
             </div>
             <div className='flexColumn'>
-            <p className='item'>When I'm not behind a computer though, I'm not even indoors. I just moved to Maryland from living my entire life in the
-                    hot and muggy Florida sun, down in Miami. I fell in love with Maryland when I visited a couple of years back and I 
-                    decided to make it my new home in 2019. Hiking, photography, and art are my go-to's when I'm not hacking away at a keyboard. This is where
-                    my love of front-end and design comes in.</p>
+            <p className='item'>When I'm not behind a computer though, I'm not even indoors. I just moved BACK to Florida from living the last 3 years
+               in DC. Hiking, photography, and art are my go-to's when I'm not hacking away at a keyboard. This is absolutely where my love of front-end 
+               and design comes in.</p>
             </div>
             </section>
             <h2 className='flexRow'>Below are my some of my favorite shots and some of my art:</h2>
